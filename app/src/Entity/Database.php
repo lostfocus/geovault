5136050aae72f92792243baac7260db50dd0ee0e
Dominik Schwind <dschwind@lostfocus.de>
2025-08-10 20:36:23 +0200

    fix(Database): The slug has to be unique

--- app/src/Entity/Database.php
+++ app/src/Entity/Database.php
@@ -18,7 +18,7 @@ class Database
      */
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $slug = null;
 
     #[ORM\Column(length: 255)]
