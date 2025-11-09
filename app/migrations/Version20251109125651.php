d184de617584a762103ad79027145c6226e07937
Dominik Schwind <dschwind@lostfocus.de>
2025-11-09 14:54:55 +0100

    feat: Write locations

---
+++ app/migrations/Version20251109125651.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251109125651 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE location (id INT AUTO_INCREMENT NOT NULL, location_database_id INT NOT NULL, timestamp_utc DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', latitude DOUBLE PRECISION NOT NULL, longitude DOUBLE PRECISION NOT NULL, content JSON NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_5E9E89CBB3BDF03D (timestamp_utc), INDEX IDX_5E9E89CBE67FEE9F (location_database_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE location ADD CONSTRAINT FK_5E9E89CBE67FEE9F FOREIGN KEY (location_database_id) REFERENCES `database` (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE location DROP FOREIGN KEY FK_5E9E89CBE67FEE9F');
+        $this->addSql('DROP TABLE location');
+    }
+}
